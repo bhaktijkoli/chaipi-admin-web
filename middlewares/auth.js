4ff0527c0e7e2c04667e27ba9b271ccd794e2074
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-06-10 12:56:42 +0530

    Init with vue js

--- middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) =>{
-    let user = firebase.auth().currentUser
-    console.log(user)
-    if(user !== null) {
-        next()
-    }
-    return res.redirect('/')
-}

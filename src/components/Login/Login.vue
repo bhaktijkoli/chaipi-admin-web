1fa59ce2329cf7b61b286d0f32b0b8d73f5a1876
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-06-20 20:52:41 +0530

    Added firebase phone config default country

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="my-5">
+  <div class="my-5">
     <h1 class="text-center my-4">Login</h1>
     <div id="firebaseui-auth-container"></div>
-    </div>
+  </div>
 </template>
 
 
@@ -11,16 +11,24 @@ import firebase from 'firebase'
 import firebaseui from 'firebaseui'
 import {config} from '../../Config/Firebase'
 export default {
-    name: 'login',
-    mounted() {
-        let uiConfig = {
-            signInSuccessUrl: '/dashboard',
-            signInOptions: [
-                firebase.auth.PhoneAuthProvider.PROVIDER_ID,    
-            ]
+  name: 'login',
+  mounted() {
+    let uiConfig = {
+      signInSuccessUrl: '/dashboard',
+      signInOptions: [
+        {
+          provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          recaptchaParameters: {
+            type: 'image', // 'audio'
+            size: 'invisible', // 'invisible' or 'compact'
+            badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
+          },
+          defaultCountry: 'IN',
         }
-        let ui = new firebaseui.auth.AuthUI(firebase.auth());
-        ui.start('#firebaseui-auth-container', uiConfig);
+      ]
     }
+    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
 }
 </script>
